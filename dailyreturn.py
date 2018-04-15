4ddd578480cb2aca6913179059e650d10d9b8eee
rk1998 <rohithk98@gmail.com>
2018-04-15 17:25:24 -0400

    added code that does expectation maximization clustering and plots silhouette score

--- dailyreturn.py
+++ dailyreturn.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.covariance import empirical_covariance
-from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
 
 
 def get_csv_data(filename, index=None):
@@ -16,8 +18,41 @@ def get_csv_data(filename, index=None):
     else:
         print("file not found")
 
+def clustering(data):
+    clustering_scores = []
+    best_cluster = 2
+    best_score = -100
+    for i in range(2, 30):
+        em = GaussianMixture(n_components=i, covariance_type='tied',
+            max_iter=1000)
+        em.fit(data)
+        score = silhouette_score(data, em.predict(data))
+        if score > best_score:
+            best_score = score
+            best_cluster = i
+        clustering_scores.append(score)
 
+    em = GaussianMixture(n_components=2, covariance_type='tied')
+    em.fit(data)
+    labels = em.predict(data)
+    covariance = em.covariances_
+    return clustering_scores, range(2,30), labels, covariance
+
+def plot_results(x_val, y_val, title, x_label, y_label):
+    plt.figure(1)
+    plt.plot(x_val, y_val, 'o-', color='b', label='Silhouette Score')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.show()
 
 
 df = get_csv_data("DailyReturn800.csv")
-print(df)
+scores, cluster_range, clusters, covariance_matrix = clustering(np.transpose(df.values))
+print(clusters)
+print(covariance_matrix)
+plot_results(cluster_range, scores,
+    "Expectation Maximization Clustering", "Number of Clusters", "Score")
+
+
