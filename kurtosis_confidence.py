e754ef901ac974d78a02040ec58ae40907836afa
Paige Rutherford <paigerutherford8@gmail.com>
2018-04-22 13:44:34 -0400

    kurtosis and confidence intervals

---
+++ kurtosis_confidence.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import os
+import scipy as sp
+import scipy.stats
+
+
+def get_csv_data(filename, index=None):
+    '''
+    Gets data from a csv file and puts it into a pandas dataframe
+    '''
+    if os.path.exists(filename):
+        print( filename + " found ")
+        data_frame = pd.read_csv(filename, index_col=index)
+        return data_frame
+    else:
+        print("file not found")
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+    return m, m-h, m+h
+
+df = get_csv_data("DailyReturn800.csv")
+for i in df.axes[1]:
+    print(scipy.stats.kurtosis(df[i]))
